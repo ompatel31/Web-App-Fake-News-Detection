3caa7492b11be6fde7ce7f73710db8368ddb14c2
Om patel <ompatel31012001@gmail.com>
2023-02-18 20:04:51 +0000

    Fake news detection project using ML and NLP

---
+++ Fake_News_Det.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+from flask import Flask, render_template, url_for
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+app = Flask(__name__)
+tfvect = TfidfVectorizer(stop_words='english', max_df=0.7)
+loaded_model = pickle.load(open('model 3.pkl', 'rb'))
+dataframe = pd.read_csv('news.csv')
+x = dataframe['text']
+y = dataframe['label']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+
+def fake_news_det(news):
+    tfid_x_train = tfvect.fit_transform(x_train)
+    tfid_x_test = tfvect.transform(x_test)
+    input_data = [news]
+    vectorized_input_data = tfvect.transform(input_data)
+    prediction_prob = loaded_model.predict_proba(vectorized_input_data)
+    prediction = loaded_model.predict(vectorized_input_data)
+    return prediction, round(prediction_prob[0][0] * 100, 2), round(prediction_prob[0][1] * 100, 2)
+
+
+@app.route('/')
+def home():
+    return render_template('Home.html')
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        message = request.form['message']
+        pred, fake_prob, real_prob = fake_news_det(message)
+        return render_template('index.html', prediction=pred, fake_prob=fake_prob, real_prob=real_prob)
+    else:
+        return render_template('index.html', prediction="Something went wrong")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
